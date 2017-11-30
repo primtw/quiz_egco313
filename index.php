223770cc74332b701cdb69ff309de6d42b6a566b
primtw <primmy500@gmail.com>
2017-11-30 08:12:47 +0000

    php

--- index.php
+++ index.php
@@ -1,27 +1,32 @@
-<?php $link = mysql_connect('localhost', 'root'); ?>
-<html>
-<head>
-	<title>Hello world!</title>
-	<style>
-	body {
-		background-color: white;
-		text-align: center;
-		padding: 50px;
-		font-family: "Open Sans","Helvetica Neue",Helvetica,Arial,sans-serif;
-	}
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "egco313";
 
-	#logo {
-		margin-bottom: 40px;
-	}
-	</style>
-</head>
-<body>
-	<img id="logo" src="logo.png" />
-	<h1><?php echo "Hello world!"; ?></h1>
-	<?php if(!$link) { ?>
-		<h2>Can't connect to local MySQL Server!</h2>
-	<?php } else { ?>
-		<h2>MySQL Server version: <?php echo mysql_get_server_info(); ?></h2>
-	<?php } ?>
-</body>
-</html>
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully <br>";
+echo "----------------------------<br>";
+
+$sql = "SELECT * FROM person";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+        echo "id: " . $row["id"]. " - Name: " . $row["name"]. "-age: " . $row["age"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+mysqli_close($conn);
+
+
+?>
